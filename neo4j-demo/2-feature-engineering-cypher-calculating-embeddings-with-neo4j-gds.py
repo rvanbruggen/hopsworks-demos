799bcabfe9852c98d4ed941367971e1f53de57b3
Rik Van Bruggen <rik@vanbruggen.be>
2024-02-21 19:30:06 +0000

    Committing new version of Neo4j demo

--- neo4j-demo/2-feature-engineering-cypher-calculating-embeddings-with-neo4j-gds.py
+++ neo4j-demo/2-feature-engineering-cypher-calculating-embeddings-with-neo4j-gds.py
@@ -5,27 +5,31 @@ URI = "bolt://localhost:7687"
 AUTH = ("neo4j", "changeme")
 DATABASE = "neo4j"
 
+
 with GraphDatabase.driver(URI, auth=AUTH) as driver:
     try:
         gds = GraphDataScience(URI, auth=AUTH)
+        
+        gds.run_cypher(
+            """
+            CALL gds.graph.drop('transaction_graph') YIELD graphName
+            """
+        )
         gds.run_cypher(
         """
         MATCH (p1:Party)-[t:TRANSACTION]->(p2:Party)
-            WHERE t.tran_timestamp >= datetime("2020-10-01")
-                AND t.tran_timestamp < datetime("2020-11-01")
+            WHERE t.tran_timestamp >= datetime("2020-11-01")
+                AND t.tran_timestamp < datetime("2020-12-01")
             RETURN p1, t, p2
         """
         )
-        G, project_result = gds.graph.project("transaction_graph", "Party", {"TRANSACTION": {"properties": ["tran_id","tx_type","base_amt","tran_timestamp"]}})
-
+        G, project_result = gds.graph.project("transaction_graph", "Party", "TRANSACTION")
         node2vec_result = gds.node2vec.write(
             G,                                #  Graph object
-            featureProperties=["community"],  #  Configuration parameters
             embeddingDimension=10,
             walkLength=80,
             inOutFactor=1,
             returnFactor=1,
-            relationshipWeightProperty="base_amt",
             writeProperty="node2vec"
         )
         assert node2vec_result["nodePropertiesWritten"] == G.node_count()
